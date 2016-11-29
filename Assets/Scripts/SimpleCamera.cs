377d14a50468cb36e0e6e637a69d3053015d4711
Danny Bauer <foreach.loop@outlook.com>
2016-11-29 12:34:50 +0100

    readded the old simple cam

---
+++ Assets/Scripts/SimpleCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleCamera : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+
+    }
+
+    //Late: runs after every object has been updated
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+        transform.LookAt(player.transform);
+
+    }
+}
