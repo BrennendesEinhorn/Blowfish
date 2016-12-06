53548a43ef16f366a8df9cb5736d320ff4e5b062
BrennendesEinhorn <selena.bobek@web.de>
2016-12-06 15:21:18 +0100

    1. Versuch Startsreen
    Da klappt so einiges noch nicht. Das Licht lässt sich nicht verändern,
    der Code funktioniert nicht obwohl im Code selbst kein Fehler ist und
    wir müssen dann noch iregendwie die MenuScene Level o nennen damit des
    geht, ich denke das müssen wir morgen dann zusammen machen

---
+++ Assets/Scripts/MouseHover Startscreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseHoverStartscreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.color = Color.black;
+	}
+	
+	// Update is called once per frame
+	void OnMouseEnter () {
+		renderer.material.color = Color.white;
+	}
+	void OnMouseExit() {
+		renderer.material.color = Color.black;
+	}
+}
