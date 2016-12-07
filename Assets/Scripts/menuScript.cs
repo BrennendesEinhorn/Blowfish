9e39d34a63e302711be1f53101c90585ae72dcc4
BrennendesEinhorn <selena.bobek@web.de>
2016-12-07 15:18:49 +0100

    Startscreen
    Wir müssen jetzt nur noch deine Scene da reinfügen, damit sie geöffnet
    wird wenn man auf play drückt

---
+++ Assets/Scripts/menuScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour {
+
+	public GameObject quitMenu;
+	public GameObject startText;
+	public GameObject exitText;
+
+	// Use this for initialization
+	void Start () {
+		quitMenu.SetActive (false);
+	}
+	
+	public void ExitPress() {
+		quitMenu.SetActive(true);
+		startText.SetActive(false);
+		exitText.SetActive(false);
+	}
+
+	public void NoPress(){
+		quitMenu.SetActive(false);
+			startText.SetActive(true);
+				exitText.SetActive(true);
+	}
+
+	public void StartLevel() {
+		SceneManager.LoadScene(1);
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+}
+
+//why is life so precious and so cruel. Design by Selena Pearl Bobek
