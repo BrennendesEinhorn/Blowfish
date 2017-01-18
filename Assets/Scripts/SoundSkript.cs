b4c19ce3226b141e2293c3148d40e8838609d5c2
Lyloooo <os-152958@ap053091.hrw.hs-weingarten.de>
2017-01-18 17:25:26 +0100

    Sound Manager

---
+++ Assets/Scripts/SoundSkript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundSkript : MonoBehaviour {
+
+    public AudioSource GrasSound;
+    public AudioSource JumpSound;
+    public AudioSource StoßSound;
+    public AudioSource WasserSound;
+    
+
+    public void PlayGrasSound ()
+    {
+        GrasSound.Play();
+    }
+
+    public void PlayJumpSound ()
+    {
+        JumpSound.Play();
+    }
+
+    public void PlayStoßSound()
+    {
+        StoßSound.Play();
+    }
+
+    public void PlayWasserSound()
+    {
+        WasserSound.Play();
+    }
+
+    
+}
