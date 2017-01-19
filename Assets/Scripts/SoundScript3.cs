0f1f00bdf8a9a90f1111c8eede6dffeec5c9e6ff
Lyloooo <os-152958@ap053091.hrw.hs-weingarten.de>
2017-01-19 13:53:04 +0100

    SoundManager2

---
+++ Assets/Scripts/SoundScript3.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundScript3 : MonoBehaviour
+{
+
+
+
+    public AudioSource GrasSound;
+    public AudioSource JumpSound;
+    public AudioSource StoßSound;
+    public AudioSource WasserblasenSound;
+    public AudioSource GameFinishSound;
+
+
+    public void PlayGrasSound()
+    {
+        GrasSound.Play();
+    }
+
+    public void PlayJumpSound()
+    {
+        JumpSound.Play();
+    }
+
+    public void PlayStoßSound()
+    {
+        StoßSound.Play();
+    }
+
+    public void PlayWasserblasenSound()
+    {
+        WasserblasenSound.Play();
+    }
+    public void PlayGameFinishSound()
+    {
+        GameFinishSound.Play();
+    }
+
+
+}
+
